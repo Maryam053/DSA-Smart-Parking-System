e2dc544d4cc4c486289ea002056072c733fcf097
Maryam053 <maryamjamshed053@gmail.com>
2026-01-11 08:50:08 +0500

    Add ParkingArea class with dynamic slot array management

---
+++ AllocationEngine.h
@@ -0,0 +1,40 @@
+#ifndef ALLOCATIONENGINE_H
+#define ALLOCATIONENGINE_H
+
+#include "Zone.h"
+#include "ParkingRequest.h"
+
+struct AllocationResult {
+    bool success;
+    int allocatedSlotID;
+    int allocatedZoneID;
+    bool isCrossZone;
+    
+    AllocationResult() {
+        success = false;
+        allocatedSlotID = -1;
+        allocatedZoneID = -1;
+        isCrossZone = false;
+    }
+};
+
+class AllocationEngine {
+private:
+    Zone* zones;
+    int zoneCount;
+
+public:
+    AllocationEngine();
+    AllocationEngine(Zone* zones, int zoneCount);
+    
+    AllocationResult allocateSlot(ParkingRequest& request);
+    bool freeSlot(int slotID, int zoneID);
+    
+    ParkingSlot* findSlotInZone(int zoneID);
+    ParkingSlot* findSlotInAdjacentZones(int requestedZoneID);
+    ParkingSlot* findSlotByID(int slotID, int zoneID);
+    
+    Zone* getZone(int zoneID);
+};
+
+#endif
